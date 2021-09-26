00a1a0b034a9409c131e50dc7e2831ea676e4971
Harshith1329 <harshithprabhakar@gmail.com>
2021-09-26 19:02:24 +0530

    first

---
+++ makemytripframework/src/main/java/makemytripframework/makemytripframework/App.java
@@ -0,0 +1,13 @@
+package makemytripframework.makemytripframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
